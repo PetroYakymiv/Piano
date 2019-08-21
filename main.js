d2dca9db73e43b127c91541020e923fa9339c0d5
Petro Yakymiv <petroyakymiv@gmail.com>
2019-08-21 21:58:23 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,6 @@
-const keys = document.querySelectorAll(".key"),
-  note = document.querySelector(".nowplaying"),
-  hints = document.querySelectorAll(".hints");
+function play(sound){
+    const audio = document.getElementById("audio");
+          audio.setAttribute('src', sound);
+          audio.play();
+ }
 
-function playNote(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`),
-    key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-
-  if (!key) return;
-
-  const keyNote = key.getAttribute("data-note");
-
-  key.classList.add("playing");
-  note.innerHTML = keyNote;
-  audio.currentTime = 0;
-  audio.play();
-}
-
-function removeTransition(e) {
-  if (e.propertyName !== "transform") return;
-  this.classList.remove("playing");
-}
-
-function hintsOn(e, index) {
-  e.setAttribute("style", "transition-delay:" + index * 50 + "ms");
-}
-
-hints.forEach(hintsOn);
-
-keys.forEach(key => key.addEventListener("transitionend", removeTransition));
-
-window.addEventListener("keydown", playNote);
