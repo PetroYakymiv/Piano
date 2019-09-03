3100f32028114381ecf9f4d046723f4e5badaba0
Petro Yakymiv <petroyakymiv@gmail.com>
2019-09-04 01:40:19 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,18 +4,9 @@ function play(sound){
           audio.play();
  }
 
-//  function playNote(n){
-//      const note = document.querySelector(`audio[data-key="${n.keyCode}"]`);
-//            note.play();
+ function playNote(n){
+    const note = document.querySelector(`audio[data-key="${n.keyCode}"]`);
+          note.play();
  
-// }
-
-
-window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector('audio[data-key="${e.keyCode}"]');
-    const key = document.querySelector('.key[data-key="${e.keyCode}"]');
-    if (!audio) return;
-    audio.currentTime = 0; 
-    audio.play();
-    key.classList.add('playing')
-});
+}
+window.addEventListener('keydown', playNote);
